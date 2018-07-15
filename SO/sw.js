aa6298521246b1b52171b09694142565b6a95235
Mateus Barbosa <mateusbarbosa@infojr.com.br>
2018-07-15 13:20:01 -0300

    Faz download do material icons

--- SO/sw.js
+++ SO/sw.js
@@ -6,11 +6,13 @@ var urlsToCache = [
     'assets/css/stylesheet.css',
     'assets/vendor/velocity/velocity.min.js',
     'assets/vendor/jQuery/jquery-3.3.1.min.js',
-    'assets/vendor/jQuery-ui-1.12.1/jquery-ui.min.js',
-    'assets/vendor/jQuery-ui-1.12.1/jquery-ui.min.css',
     'assets/vendor/materialize/js/materialize.min.js',
+    'assets/vendor/jQuery-ui-1.12.1/jquery-ui.min.js',
+    'assets/vendor/materialize/css/material-icons.css',
+    'assets/vendor/jQuery-ui-1.12.1/jquery-ui.min.css',
     'assets/vendor/materialize/css/materialize.min.css',
-    'assets/vendor/jQuery-Mask-Plugin/dist/jquery.mask.min.js'
+    'assets/vendor/jQuery-Mask-Plugin/dist/jquery.mask.min.js',
+    'assets/vendor/materialize/css/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
 ];
 
 self.addEventListener('install', function(event) {
