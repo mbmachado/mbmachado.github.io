093899f33ba26212f250d0e139dbb0bd97dee2f5
Mateus Barbosa <mbmachado@outlook.com>
2021-09-29 11:56:39 -0300

    Adiciona evento do analytics em link externo

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById('external-link').addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const url = 'https://google.com.br';
+  
+  ga('create', 'UA-208804201-1', 'mbmachado.github.io', 'gaTracker' );
+  ga('gaTracker.send', 'event', 'Whatsapp', 'Botao', 'Rodape', url, {
+    'transport': 'beacon',
+    'hitCallback': function(){ 
+      window.open(url, '_blank');
+    }
+  });
+});
